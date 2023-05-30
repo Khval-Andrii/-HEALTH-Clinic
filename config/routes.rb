240604ea2d0b4fe61a00e43c71004b5294881569
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-05-30 12:49:00 +0200

    add new models and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'main/index'
+  devise_for :doctors
+  devise_for :users
+
+  resourses :categories
+  resourses :appointment do
+    resourses :recommandation
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # root "articles#index"
+  root "main#index"
 end
