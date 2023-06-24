7555e4a451814dc966b0612c14518533822dbaeb
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-06-24 13:17:44 +0200

    changed Dockerfile

--- app/admin/doctors.rb
+++ app/admin/doctors.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Doctor do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :phone, :name, :category_id
+  permit_params :encrypted_password, :reset_password_sent_at, :remember_created_at, :phone, :name, :category_id
   #
   # or
   #
